be0cdd48bba68665a7d446fbcabc67bb427ca8d4
Sam Lynch <sam.lynch2@hotmail.co.uk>
2017-05-18 00:50:29 +0100

    Initial commit

---
+++ Assets/Scripts/GameMaster.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Networking;
+
+public class GameMaster : NetworkManager
+{
+    public enum PLAYERS
+    {
+        P1,
+        P2
+    }
+
+    static List<int> Deck = new List<int>(11);
+    static List<int> P1Hand = new List<int>();
+    static List<int> P2Hand = new List<int>();
+    public PLAYERS currentTurn = PLAYERS.P1;
+
+    static public Canvas CANVAS;
+
+    private void Start()
+    {
+        for (int i = 1; i < 12; i++)
+        {
+            Deck.Add(i);
+        }
+        ShuffleDeck();
+        CANVAS = GameObject.Find("Canvas").GetComponent<Canvas>();
+    }
+
+    static void ShuffleDeck()
+    {
+        int n = Deck.Count;
+        while (n > 0)
+        {
+            n--;
+            int k = Random.Range(0, Deck.Count-1);
+            var value = Deck[k];
+            Deck[k] = Deck[n];
+            Deck[n] = value;
+        }
+    }
+
+    public override void OnStopServer()
+    {
+        GameObject[] cards = GameObject.FindGameObjectsWithTag("Card");
+
+        for (int i = 0; i < cards.Length; i++)
+        {
+            Destroy(cards[i]);
+        }
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Networking;
+
+public class Player : NetworkBehaviour
+{
+    GameMaster.PLAYERS PLAYER;
+    public GameObject CardPrefab;
+
+    public List<GameObject> Hand;
+
+    void Start()
+    {
+        this.transform.SetParent(GameMaster.CANVAS.transform);
+        this.transform.localScale = new Vector3(1, 1, 1);
+    }
+
+    void Update()
+    {
+        if (!isLocalPlayer)
+        {
+            return;
+        }
+        
+
+    }
+
+    public override void OnStartLocalPlayer()
+    {
+        GameObject[] players = GameObject.FindGameObjectsWithTag("Player");
+        if (players.Length % 2 == 1)
+            PLAYER = GameMaster.PLAYERS.P1;
+        else
+            PLAYER = GameMaster.PLAYERS.P2;
+
+        Hand = new List<GameObject>();
+        Hand.Add(GameObject.Instantiate<GameObject>(CardPrefab, GameMaster.CANVAS.transform));
+        Hand[0].GetComponent<Card>().player = Network.player.guid;
+
+        if(PLAYER == GameMaster.PLAYERS.P1)
+        {
+            Hand[0].GetComponent<RectTransform>().anchorMin.Set(0, 0);
+            Hand[0].GetComponent<RectTransform>().anchorMax.Set(0, 0);
+
+            foreach (GameObject card in Hand)
+            {
+                card.GetComponent<Image>().color = Color.green;
+                card.GetComponentInChildren<Text>().text = "P1";
+            }
+        }
+        else
+        {
+            Hand[0].GetComponent<RectTransform>().anchorMin.Set(0, 1);
+            Hand[0].GetComponent<RectTransform>().anchorMax.Set(0, 1);
+            Hand[0].GetComponent<RectTransform>().offsetMin.Set(75, -75);
+            Hand[0].GetComponent<RectTransform>().offsetMax.Set(75, -75);
+
+            foreach (GameObject card in Hand)
+            {
+                card.GetComponent<Image>().color = Color.red;
+                card.GetComponentInChildren<Text>().text = "P2";
+            }
+        }
+    }
+}
